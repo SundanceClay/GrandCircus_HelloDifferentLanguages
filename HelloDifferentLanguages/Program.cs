389b71529005de008cb2b58f7cb685ce04f11fda
Shawn Penning <penningsp@hotmail.com>
2022-02-03 19:12:18 -0500

    Final Hello World in many languages.

--- HelloDifferentLanguages/Program.cs
+++ HelloDifferentLanguages/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string userLoop = "y";
+do
+{
+    Console.WriteLine("Enter a language or n to stop: ");
+    string userInput = (Console.ReadLine()).ToLower();
+
+    switch (userInput) 
+    {
+        case "english":
+            Console.WriteLine("Hello World!");
+            break;
+        case "spanish":
+            Console.WriteLine("Hola Mundo!");
+            break;
+        case "french":
+            Console.WriteLine("Bonjour le monde!");
+            break;
+        case "n":
+            userLoop = "n";
+            break;
+        default:
+            Console.WriteLine("I don't know that one!");
+            break;
+    }
+}
+while (userLoop != "n");
+
